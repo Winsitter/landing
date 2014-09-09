35da10c22f97ae703b86a5218c50fba68e4a183a
Kevin Griffin <kevin@kevgriffin.com>
2014-09-09 10:37:58 -0400

    ignore sitemap fix

--- build.js
+++ build.js
@@ -45,7 +45,7 @@ Metalsmith(__dirname)
     destination: './' // relative to the build directory
   }))
   .use(sitemap({
-    ignoreFiles: [/feed.xml/], // Matched files will be ignored
+    ignoreFiles: [/(.*.png)|(.*.gif)|(.*.jpg)|(.*.css)|(.*.xml)|(.*.js)|(.*.ico)/], // Matched files will be ignored
     output: 'sitemap.xml', // The location where the final sitemap should be placed
     urlProperty: 'path', // Key for URL property
     modifiedProperty: 'modified', // Key for last modified property
